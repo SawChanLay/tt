97e11051587cdf79671cd62bad11ea52482d62c3
Aung Chan Myint <sawacm97@gmail.com>
2020-02-25 16:54:27 +0630

    fixed search box

--- tt/js/script.js
+++ tt/js/script.js
@@ -4,5 +4,14 @@ jQuery(document).ready(function() {
     jQuery( ".social-icon-search" ).click(function() {
         jQuery('.search-form').slideToggle(500);
     });
+
+    //! Focus In/ Out
+    jQuery(".search-box-btn").css("pointer-events", "none");
+    jQuery(".search-box-input").focusin(function() {
+        jQuery(".search-box-btn").css("pointer-events", "auto");
+    });
+    jQuery(".search-box-input").focusout(function() {
+        jQuery(".search-box-btn").css("pointer-events", "none");
+    });
 });
 
