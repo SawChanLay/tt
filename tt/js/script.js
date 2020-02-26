31f022c41229f8b01114139c8dcb1ea060220e66
Aung Chan Myint <sawacm97@gmail.com>
2020-02-26 13:48:21 +0630

    modified main menu

--- tt/js/script.js
+++ tt/js/script.js
@@ -13,5 +13,22 @@ jQuery(document).ready(function() {
     jQuery(".search-box-input").focusout(function() {
         jQuery(".search-box-btn").css("pointer-events", "none");
     });
+
+    //! AUTOCOMPLETE-OFF
+    jQuery(".search-box input[type='text']").attr("autocomplete", "off");
+
+    //! FORM PLACEHOLDER
+    jQuery(".sing-up input[name='name']").focusin(function(){
+        jQuery(this).attr('placeholder','');
+    });
+    jQuery(".sing-up input[name='name']").focusout(function(){
+        jQuery(this).attr('placeholder', 'Name');
+    });
+    jQuery(".sing-up input[name='email']").focusin(function(){
+        jQuery(this).attr('placeholder','');
+    });
+    jQuery(".sing-up input[name='email']").focusout(function(){
+        jQuery(this).attr('placeholder', 'Email*');
+    });
 });
 
